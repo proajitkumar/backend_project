4f603ee2ed1053b6fccb42caa7764b76ab7e196c
eazyajitkumar <pro.ajitkumar@gmail.com>
2026-05-30 00:51:56 +0530

    initial template done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const { PORT } = require("./config");
-
-const { AboutController, HomeController } = require("./controllers");
-
 const app = express();
+
+const apiRoutes = require("./routes");
+
+app.use("/api", apiRoutes);
+
 app.listen(PORT, () => {
   console.log(`Successfully started the server on PORT : ${PORT}`);
 });
