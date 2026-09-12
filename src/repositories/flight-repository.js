5f62ef1f7aff63b6c7e24e71b42550806722ca29
Ajit Kumar <88327747+proajitkumar@users.noreply.github.com>
2026-09-12 16:25:27 +0530

    included Airplane,departureAirport, arrivalAirport and City with getAllFlight API

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository = require("./crud-repository");
-const { Flight } = require("../models");
+const { Flight, Airplane, Airport, City } = require("../models");
+const { Op, Sequelize } = require("sequelize");
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -10,6 +11,46 @@ class FlightRepository extends CrudRepository {
     const response = await Flight.findAll({
       where: filter,
       order: order,
+      include: [
+        {
+          model: Airplane,
+          required: true,
+          as: "airplaneDetail",
+        },
+        {
+          model: Airport,
+          required: true,
+          as: "departureAirport",
+          on: {
+            col1: Sequelize.where(
+              Sequelize.col("Flight.departureAirportId"),
+              "=",
+              Sequelize.col("departureAirport.code"),
+            ),
+          },
+          include: {
+            model: City,
+            required: true,
+          },
+        },
+        {
+          model: Airport,
+          required: true,
+          as: "arrivalAirport",
+          on: {
+            col1: Sequelize.where(
+              Sequelize.col("Flight.arrivalAirportId"),
+              "=",
+              Sequelize.col("arrivalAirport.code"),
+            ),
+          },
+
+          include: {
+            model: City,
+            required: true,
+          },
+        },
+      ],
     });
     return response;
   }
